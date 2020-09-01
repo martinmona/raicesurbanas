2e160457086e7bf7086fc9242c027a1b8bc90c2b
unknown <you@example.com>
2020-09-01 11:32:32 -0300

    textos finales y detalles css

--- contact_me.php
+++ contact_me.php
@@ -16,7 +16,7 @@ $option = strip_tags(htmlspecialchars($_POST['option']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'pipi.tdfuego@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'info@raicesurbanas.com.ar'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Contacto desde web Raices Urbanas:  $name";
 $email_body = "Has recibido un mensaje desde la web de raices urbanas\n\n"."Nombre: $name\n\nEmail: $email_address\n\nOpcion: $option\n\nMensage: $message";
 $headers = "From: noreply@raicesurbanas.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
